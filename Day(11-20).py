8b009d44b8a5dd3ca43329bd491196e6e55aec70
vaibhavb-debug <118048807+vaibhavb-debug@users.noreply.github.com>
2023-10-04 15:13:51 +0530

    Update Day(11-20).py

--- Day(11-20).py
+++ Day(11-20).py
@@ -1,1 +1,19 @@
+#Day 11
+name = "Vaibhav"
+friend = "Kajal"
+anotherFriend = 'Shubhangi'
+apple = '''He said,
+Hi Vaibhav
+Hey I am good
+"I want to eat an apple'''
+print("Hello, " + name)
+print("Hello, " + friend)
+print("Hello, " + anotherFriend)
 
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
