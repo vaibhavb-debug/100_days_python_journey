2f346af3ee471bcbaa9df44a28cfd2e4733366be
vaibhavb-debug <118048807+vaibhavb-debug@users.noreply.github.com>
2023-10-04 14:18:31 +0530

    Update Day(1-10).py

--- Day(1-9).py
+++ Day(1-9).py
@@ -1,1 +1,79 @@
-jgk
+# Day 1
+
+
+print('Hello World')
+
+print(4)
+
+# Day 2 
+# Introduction & Installation
+
+#Day 3
+
+#Day 4
+
+print("Hello World")
+
+print(7*8)
+
+ # Day_5
+
+print("Hey I am good boy\nand this viewer is also good boy")
+
+print("Hey", 6, 7, sep="~", end="009\n")
+print("Vaibhav")
+
+# Day_6
+
+#Variable and data type
+a= complex(8, 2)
+b= True
+c= "Kajal"
+d= None
+
+print(a)
+print(b)
+
+a1 = 9
+print(a+a1)
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+
+# Day_7
+
+# Arithematic Operators
+print(5+6)
+print(7-5)
+print(15*6)
+print(15/6)
+print(15//6)
+print(7%2)
+print(2**3)
+
+# Calculator
+
+a=50
+b=3
+print("The Value of ", a,"+",b ,"is : ", a+b)
+print("The Value of ", a,"-",b ,"is : ", a-b)
+print("The Value of ", a,"*",b ,"is : ", a*b)
+print("The Value of ", a,"/",b ,"is : ", a/b)
+
+#Type_Casting
+#Explicit Type_Casting
+
+x= "1"
+y= "2"
+print("Type Casting str to int : ",int(x)+int(y))
+
+# Implicit Type_casting
+c = 1.9
+d = 8
+print("Implicit Type Casting : ",c + d) # automatically converted d also to float
+
+# Day 10
+
+x = input("Enter the first No : ")
+y = input("Enater the second No : ")
+print(int(x)+int(y))
