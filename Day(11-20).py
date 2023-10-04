566b3123ffcc6afb7d904cb09347d4a59dc47903
vaibhavb-debug <118048807+vaibhavb-debug@users.noreply.github.com>
2023-10-04 15:20:56 +0530

    Update Day(11-20).py

--- Day(11-20).py
+++ Day(11-20).py
@@ -17,3 +17,11 @@ print(name[3])
 print(name[4])
 print(name[5])
 print(name[6])
+
+# reading the string using the for loop 
+print("Let us use the for Loop\n")
+for character in name:
+    print(character)
+
+
+
